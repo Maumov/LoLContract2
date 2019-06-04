6f1743c02b75fa796d5e31cba996659fe3c7cf12
Antonio <antonyurieta@hotmail.com>
2019-06-04 17:10:51 -0500

    .

---
+++ Assets/Scripts/AnimatorController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour
+{
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void PlayIdle()
+    {
+        animator.Play("Idle");
+    }
+
+    public void PlayStartAttack()
+    {
+        // For player: dodge the attack
+        // From IdleAttack or Idle to Start Attack
+        animator.SetTrigger("StartAttack");
+    }
+    public void PlayEndAttack()
+    {
+        // Attack after dodging the boss.
+        // Since the "time" will be frozen at the end of StartAttack Animation, this one is going to "REStart" the time flow.
+        animator.SetTrigger("EndAttack");
+    }
+
+    public void PlayDamaged()
+    {
+        // Once it takes damage.
+        animator.SetTrigger("Damaged");
+    }
+
+    public void PlayDefeated()
+    {
+        // Once it takes damage.
+        animator.SetTrigger("Defeated");
+    }
+}
