e8ff80f3c34afc98dcf94b3d095dc9e84f48c563
Mauricio <mau.movilla@gmail.com>
2019-06-06 18:18:42 -0500

    animaciones, player camera, player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public float rotationSpeed;
+
+
+
+    float vertical;
+    float horizontal;
+
+    CharacterController characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInputs();   
+        Move();
+    }
+
+    void GetInputs() {
+        vertical = Input.GetAxisRaw("Vertical");
+        horizontal = Input.GetAxisRaw("Horizontal");
+    }
+
+    void Move() {
+        Vector3 dir = new Vector3(horizontal,0f, vertical);
+        dir.Normalize();
+        characterController.Move(dir * movementSpeed * Time.deltaTime);
+        transform.LookAt(transform.position + characterController.velocity);
+    }
+}
