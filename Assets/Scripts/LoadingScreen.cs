11e0f5cb2eb691761dce0d63e74cc0e195fc6e1c
Mauricio <mau.movilla@gmail.com>
2019-06-11 17:17:34 -0500

    fade

--- Assets/Scripts/LoadingScreen.cs
+++ Assets/Scripts/LoadingScreen.cs
@@ -8,8 +8,12 @@ public class LoadingScreen : MonoBehaviour
 {
     public string scene;
 
+    public Image fadeImage;
+    public float fadeSpeed;
     private void Start()
     {
+
+        StartCoroutine(FadeIn());
     }
 
     public void ChangeLevel()
@@ -19,9 +23,28 @@ public class LoadingScreen : MonoBehaviour
 
     IEnumerator LoadBackgroundScene()
     {
+        yield return StartCoroutine(FadeOut());
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
         while (!asyncLoad.isDone)
         {
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeOut() {
+        float fade = 0f;
+        while(fade <= 1f) {
+            fade += Time.deltaTime * fadeSpeed;
+            fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, fade);
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeIn() {
+        float fade = 1f;
+        while(fade >= 0f) {
+            fade -= Time.deltaTime * fadeSpeed;
+            fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, fade);
             yield return null;
         }
     }
