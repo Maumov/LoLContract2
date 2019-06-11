f28217c9680df7ca2f64023e6be0c9c2e7be63c2
Mauricio <mau.movilla@gmail.com>
2019-06-10 22:41:11 -0500

    lifebar working

---
+++ Assets/LifeBarViewer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeBarViewer : MonoBehaviour
+{
+    public bool isPlayer;
+    Stats stats;
+    Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Combat[] statss = FindObjectsOfType<Combat>();
+
+        foreach(Combat s in statss) {
+            if(s.isPlayer == isPlayer) {
+                s.GetComponent<Stats>().OnDamageReceived += UpdateLifeBar;
+            }
+        }
+        slider = GetComponent<Slider>();
+
+    }
+    
+    void UpdateLifeBar() {
+        slider.value = stats.currentHitPoints / stats.maxHitPoints;
+    }
+}
