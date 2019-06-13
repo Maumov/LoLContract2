16579a0a3d6d0d466a2de7902ca7177cc3cae6c1
Renato <renmazzilli@gmail.com>
2019-06-13 09:23:15 -0500

    Added TextDisplayPrefab with their scripts
    Revisar si los scripts estan acorde a lo que se hara en este proyecto o se haran scripts nuevos

---
+++ Assets/Scripts/Hud/MessagesController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessagesController : MonoBehaviour
+{
+    public TextController textPrefab;
+    public GameObject canvas;
+
+
+    public void SpawnText(List<Frases> keyNames)
+    {
+        canvas.SetActive(true);
+        List<Frases> list = new List<Frases>();
+        list.AddRange(keyNames);
+        if(list != null)
+        {
+            textPrefab.SetText(list);
+        }
+    }
+}
---
+++ Assets/Scripts/Hud/TextController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TextController : MonoBehaviour
+{
+    public Queue<Frases> message;
+    public TextMeshProUGUI text;
+    public TextMeshProUGUI buttonText;
+    public bool completed;
+    public float delay = 1;
+    private float delayCounter;
+
+    UnityEvent eventoActual;
+
+    public GameManager gameManager;
+
+    private void Start()
+    {
+        delayCounter = delay;
+    }
+
+    public void UpdateMessage()
+    {
+        if(delayCounter <= Time.time)
+        {
+            if(eventoActual != null) {
+                eventoActual.Invoke();
+            }
+            if (!completed)
+            {
+                eventoActual = message.Peek().evento;
+                string word = message.Dequeue().key;
+                text.text = SharedState.LanguageDefs[word];
+                if (message.Count == 0)
+                {
+                    completed = true;
+                    buttonText.text = SharedState.LanguageDefs["ok"];
+                }
+            }
+            else
+            {
+                gameManager.TutorialFinish();
+                TurnOffThisObject();
+            }
+
+            delayCounter = Time.time + delay;
+        }
+
+    }
+
+    public void SetText(List<Frases> data)
+    {
+        eventoActual = null;
+        completed = false;
+        message = new Queue<Frases>(data);
+        buttonText.text = SharedState.LanguageDefs["next"];
+        UpdateMessage();
+        
+    }
+
+    public void AddText(Frases data) {
+        message.Enqueue(data);
+    }
+
+
+    public void TurnOffThisObject()
+    {
+        transform.parent.gameObject.SetActive(false);
+    }
+
+    public void MakeUncompleted()
+    {
+        completed = false;
+    }
+}
