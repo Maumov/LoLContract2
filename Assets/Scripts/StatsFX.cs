fe80e3ea8b27b8e74d993de0b661629213805397
Mauricio <mau.movilla@gmail.com>
2019-06-13 13:12:27 -0500

    stats FX

---
+++ Assets/Scripts/StatsFX.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsFX : MonoBehaviour
+{
+
+    Stats stats;
+    public ParticleFX DamageFX;
+    public ParticleFX DeadFX;
+    public ParticleFX ReviveFX;
+
+    
+    void Start()
+    {
+        stats = GetComponent<Stats>();
+        stats.OnDamageReceived += Damage;
+        stats.OnDead += Dead;
+        stats.OnRevive += Revive;
+    }
+
+    void Damage() {
+        InstantiateParticle(DamageFX);
+    }
+
+    void Dead() {
+        InstantiateParticle(DeadFX);
+    }
+
+    void Revive() {
+        InstantiateParticle(ReviveFX);
+    }
+
+    void InstantiateParticle(ParticleFX FX) {
+        Instantiate(FX.particle, FX.positionOffset, Quaternion.identity, transform);
+    }
+
+}
+
+[System.Serializable]
+public class ParticleFX {
+    public GameObject particle;
+    public Vector3 positionOffset;
+}
+
