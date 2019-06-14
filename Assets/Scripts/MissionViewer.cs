8c0a23501d57535323d05ba2bf0c3127631815fd
Mauricio <mau.movilla@gmail.com>
2019-06-14 15:42:19 -0500

    Intro Guion, Language Modifications

---
+++ Assets/Scripts/MissionViewer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class MissionViewer : MonoBehaviour
+{
+    public List<string> textos;
+    public int currentShowing;
+    TextMeshPro text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void SetTexts(List<string> t) {
+        textos = new List<string>();
+        textos.AddRange(t);
+        currentShowing = 0;
+        Show();
+    }
+
+    public void ButtonPressed() {
+        currentShowing++;
+        if(currentShowing >= textos.Count) {
+            Hide();
+        } else {
+            Show();
+        }
+    }
+
+    void Show() {
+        text.text = SharedState.LanguageDefs[textos[currentShowing]];
+    }
+
+    void Hide() {
+        gameObject.SetActive(false);
+    }
+}
