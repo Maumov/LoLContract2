ae05cbb13a54d0555ed3a7ebaea3a93eacc01c0b
Antonio <antonyurieta@hotmail.com>
2019-06-14 14:34:33 -0500

    Eliminada mi empanada

--- Assets/Scripts/Question/QuestionHandler.cs
+++ Assets/Scripts/Question/QuestionHandler.cs
@@ -8,7 +8,6 @@ public class QuestionHandler : MonoBehaviour
     public event delegateQuestion OnAnswerReceived, OnCorrect, OnWrong, OnQuestionSet;
 
     public InfoTutorial infoTutorial;
-    public Stats player;
     public Question question;
 
     public AnswerHandler answerHandler;
